ba5c4e6c4a8b2e6f117a65545bdc55579b4af2f8
Shreyansh <dhruvpatelmegic@gmail.com>
2024-02-23 11:25:03 +0530

    run  electron project successfully

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Allows access to Node.js APIs in the renderer process
+    }
+  });
+
+  mainWindow.loadURL('http://localhost:4200'); // Assuming your Angular app is served on localhost:4200
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
